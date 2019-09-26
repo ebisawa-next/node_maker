8e0ff0fc1d967d40b5c2a55446b5d28b740b7960
abcmPrivate <MoekoEbisawa@gmail.com>
2019-09-26 23:54:46 +0900

    顔のスクリーンショットはjs側でサイズ制御する

--- src/js/pages/maker/createButton.js
+++ src/js/pages/maker/createButton.js
@@ -1,10 +1,17 @@
 export default () => {
     (() => {
-        const faceframe = document.querySelector('#prg-canvas-faceframe')
+        const faceframe = document.querySelector('#prg-canvas')
         const createButton = document.querySelector('#prg-createButton');
         createButton.addEventListener('click', async () => {
             // アイコンを作ります
-            const canvas = await html2canvas(faceframe)
+            const offset = faceframe.getBoundingClientRect()
+
+            const canvas = await html2canvas(faceframe, {
+                width: 200,
+                height: 200,
+                x: offset.left + 50,
+                y: offset.top + 20
+            })
             const imgData = canvas.toDataURL()
             document.querySelector('#prg-result-icon').src = imgData
             document.querySelector('#prg-result-download').href = imgData
