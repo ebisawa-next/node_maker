556f8e79d9593d8d046ab342b3334be56d282ad7
Moeko Ebisawa [LIFULL] <ebisawam@PCZ03500.local>
2019-09-27 11:09:05 +0900

    子かてごりも選べるようにする

--- src/js/pages/maker/selectCategory.js
+++ src/js/pages/maker/selectCategory.js
@@ -1,6 +1,27 @@
 export default () => {
-    (() => {
-        
+    const selectCategories = document.querySelectorAll('.prg-selectCategory')
+    selectCategories.forEach((selectCategory) => {
+        const parent = selectCategory.dataset.parent
+        const children = selectCategory.querySelectorAll('.prg-selectCategory-child')
+        children.forEach((child) => {
+            child.addEventListener('click', () => {
+                const currentCategory = child.dataset.category
+                // 元になるフレーム探す
+                const frame = document.querySelector(`.prg-parts-frame-${parent}`)
+
+                const frs = frame.querySelectorAll('.prg-selectParts')
+                frs.forEach((f) => {
+                    f.classList.remove('is-selected')
+                    if(f.dataset.category == currentCategory) {
+                        f.classList.add('is-selected')
+                    }
+                })
+
+
+            }, false)
+        })
     })
 
 }
+
+//波動拳になってしまう
